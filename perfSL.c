52a7e244a10a833081766bca1edf5036c2a60da2
root <root@pedro-notebook>
2024-10-04 23:28:42 -0300

    triangulariza aparentemente funciona

--- perfSL.c
+++ perfSL.c
@@ -1,41 +1,60 @@
 #include <stdio.h>
 #include "utils.h"
-#include <likwid.h>
+//#include <likwid.h>
 
-// Encontra o maior valor na matriz A a partir da linha i
-// E retorna seu índice
-int_t encontraMax(real_t **A, int_t i, int_t n)
+// Encontra o maior valor da coluna i na matriz A
+// E retorna o índice da linha aonde está
+int_t encontraMaxColuna(real_t **A, int_t i, int_t n)
 {
-    real_t maior = A[i][0];
+    real_t maior = A[0][i];
+    int_t indiceMaior = 0;
 
-    for (int j = i; j < n; j++)
-        for (int k = 0; k < n; k++)
-            if (A[j][k] > maior)   // Comparação entre floats ---- MUDAR
-                maior = A[j][k];
-    
-    printf("%lf\n", maior);
-    return maior;
+    for (int j = 0; j < n; j++)
+    {
+        if (fabs(A[j][i]) > fabs(maior))
+        {
+            maior = A[j][i];
+            indiceMaior = j;
+        }
+    }
+
+    //printf("%ld\n", indiceMaior);
+    return indiceMaior;
 }
 
 // Troca a linha inteira do iPivo com a linha do i
 // E deixa o número do iPivo por primeiro
-void trocaLinha(real_t **A, real_t *B, int_t i, int_t iPivo)
+void trocaLinha(real_t **A, real_t *B, int_t i, int_t iPivo, int_t n)
 {
+    real_t temp;
 
+    // Troca em A
+    for (int j = 0; j < n; j++)
+    {
+        temp = A[iPivo][j];
+        A[iPivo][j] = A[i][j];
+        A[i][j] = temp;
+    }
+
+    // Troca em B
+    temp = B[iPivo];
+    B[iPivo] = B[i];
+    B[i] = temp;
 }
 
 void triangulariza_gauss(real_t **A, real_t *B, int_t n)
 {
     for (int i = 0; i < n; i++)
     {
-        int_t iPivo = encontraMax(A, i, n);
+        int_t iPivo = encontraMaxColuna(A, i, n);
 
         if (i != iPivo)
-            trocaLinha(A, B, i, iPivo);
+            trocaLinha(A, B, i, iPivo, n);
 
         for(int k = i + 1; k < n; k++)
         {
-            real_t m = A[k][i] = 0.0;
+            real_t m = A[k][i] / A[i][i];
+            A[k][i] = 0.0;
 
             for (int j = i + 1; j < n; j++)
                 A[k][j] -= A[i][j] * m;
@@ -50,16 +69,10 @@ int main()
     
     scanf("%ld", &n);
 
-    real_t A[n][n];
-    real_t B[n];
-
-    // real_t **A;
-    // real_t *B;
-
-    // B = malloc(sizeof(real_t) * n);
-    // A = malloc(sizeof(real_t*) * n);
-    // for (int i = 0; i < n; i++)
-    //     A = malloc(sizeof(real_t) * n);
+    real_t **A = (real_t **)malloc(n * sizeof(real_t *));
+    for (int i = 0; i < n; i++)
+        A[i] = (real_t *)malloc(n * sizeof(real_t));
+    real_t *B = (real_t *)malloc(n * sizeof(real_t));
 
     for (int i = 0; i < n; i++)
     {
@@ -69,13 +82,25 @@ int main()
         scanf("%lf", &B[i]);
     }
 
-    // for (int i = 0; i < n; i++)
-    // {
-    //     for (int j = 0; j < n; j++)
-    //         printf("%lf ", A[i][j]);
+    triangulariza_gauss(A, B, n);
+
+    // Retrossubstituição
+
+   // Imprime saída 
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            printf("%lf ", A[i][j]);
         
-    //     printf("= %lf\n", B[i]);
-    // }
+        printf("= %lf\n", B[i]);
+    }
+
+    // Frees
+    for (int i = 0; i < n; i++)
+        free(A[i]);
+    free(A);
+    free(B);
 
     return 0;
 }
